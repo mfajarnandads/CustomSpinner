45ef686e18a353b45ee3b13e74e535b5db2b385b
USER <nandaxd1140@gmail.com>
2022-06-21 21:43:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customspinner/AdapterKabKota.kt
@@ -0,0 +1,31 @@
+package com.example.customspinner
+
+import android.content.Context
+import android.media.tv.TvContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class AdapterKabKota(
+    aplicationContext: Context,
+    private val logo: IntArray,
+    private val nama: Array<String>
+): BaseAdapter() {
+    val inflater = LayoutInflater.from(aplicationContext)
+
+    override fun getCount() = logo.size
+    override fun getItem(position: Int) = null
+    override fun getItemId(position: Int)= 0L
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val cv = inflater.inflate(R.layout.layout_item_spinner, null)
+        val imglogo = cv.findViewById<ImageView>(R.id.imgLogo)
+        val tvNama = cv.findViewById<TextView>(R.id.tvNama)
+        imglogo.setImageResource(logo[position])
+        tvNama.text = nama[position]
+        return cv
+    }
+}
